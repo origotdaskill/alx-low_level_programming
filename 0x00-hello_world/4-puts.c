a65c946fea56eacab17567c5f8ff0c0766673857
origotdaskill <mr.walid.ori@gmail.com>
2023-08-11 06:34:34 -0700

    new commit 0x04

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,6 +7,6 @@
  * @return 0 indicating successful execution.
  */
 int main(void) {
-puts("\"Programming is like building a multilingual puzzle\,");
+puts("\"Programming is like building a multilingual puzzle,");
 return (0);
 }
