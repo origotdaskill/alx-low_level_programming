284fe9616e773979c2416ed9e3137b47f7a6bf97
origotdaskill <mr.walid.ori@gmail.com>
2023-08-11 06:48:17 -0700

    new commit 0x04

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+
 /**
  * main - Entry point of the program.
  *
  * This function demonstrates the use of the puts function to print a message.
  *
  * Return: Always 0 (Success)
- *   This function returns 0 to indicate successful execution.
+ *         This function returns 0 to indicate successful execution.
  */
-int main(void) 
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+    puts("\"Programming is like building a multilingual puzzle,");
+    return 0;
 }
