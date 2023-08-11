a37f7bea91048ff2dd14ce5aa494e6c5a71834e5
origotdaskill <mr.walid.ori@gmail.com>
2023-08-11 06:56:50 -0700

    new commit 0x05

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program.
+ *
+ * This program prints a specific message using the printf function.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return 0;
+return (0);
 }
