a790b52d6b29d90bc1878ce84b25646e08442ff3
origotdaskill <mr.walid.ori@gmail.com>
2023-08-12 09:59:11 -0700

    new commit 0x02

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,12 +23,12 @@ int main(void)
 	printf("Last digit of %d is %d ", n, lastDigit);
 	if (lastDigit > 5)
 	{
-	printf("and is greater than 5\n");}
+	printf("and is greater than 5\n"); }
 	else if (lastDigit == 0)
 	{
-	printf("and is 0\n");}
+	printf("and is 0\n"); }
 	else
 	{
-	printf("and is less than 6 and not 0\n");}
+	printf("and is less than 6 and not 0\n"); }
 	return (0);
 }
