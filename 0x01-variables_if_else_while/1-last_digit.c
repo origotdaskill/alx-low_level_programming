794652d5126d5af0a6e76b205da4f813d7794d23
origotdaskill <mr.walid.ori@gmail.com>
2023-08-12 09:29:27 -0700

    new commit 0x02

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+ * main - Entry point of the program
+ *
+ * Description: Generates a random number and determines if it's positive,
+ *              negative, or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	int lastDigit = n % 10;
+	printf("Last digit of %d is %d ", n, lastDigit);
+    	if (lastDigit > 5) {
+        printf("and is greater than 5\n");
+    	} else if (lastDigit == 0) {
+        printf("and is 0\n");
+    	} else {
+        printf("and is less than 6 and not 0\n");
+    	}
+	return (0);
+}
