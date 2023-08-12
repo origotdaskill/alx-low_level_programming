e801ccdba4704a4d6982ec6b4c36a5aac7078b43
origotdaskill <mr.walid.ori@gmail.com>
2023-08-12 09:05:45 -0700

    new commit 0x00ls

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,14 +5,15 @@
 
 /* betty style doc for function main goes there */
 int main(void)
+	/*This program generates a random number and determines if it's positive, negative, or zero.*/
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	printf("The number: %d\n", n);
-   	if (n > 0) {printf("is positive\n");}
+	printf("%d ", n);
+   	if (n > 0) {printf(" is positive\n");}
 	else if (n == 0) {printf("is zero\n");}
 	else {printf("is negative\n");}
 	return (0);
