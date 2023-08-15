8e1817d0579c839abf75a5a0b9e57de76ac7aa1e
origotdaskill <mr.walid.ori@gmail.com>
2023-08-15 08:24:56 -0700

    new commit 0x04

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,16 +2,17 @@
 
 int main(void)
 {
-    char letter;
-
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-        if (letter != 'q' && letter != 'e')
-        {
-            putchar(letter);
-        }
-    }
-    putchar('\n');
+	char letter;
+	
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	/* Code to output the character goes here */
+		if (letter != 'q' && letter != 'e')
+	{
+	putchar(letter);
+	}
+	}
+	putchar('\n');
 
     return (0);
 }
