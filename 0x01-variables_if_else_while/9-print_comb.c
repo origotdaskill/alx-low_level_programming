2ec058b1fd2ed23d0f6b02b133d45e80e144d790
origotdaskill <mr.walid.ori@gmail.com>
2023-08-15 16:33:26 -0700

    new commit 0x09

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,25 +6,18 @@
  * Description: a program that prints all possible combinations of single-digit numbers.
  *
  * Return: Always 0 (Success)
- */
+.*/
 int main(void)
 {
-	int comma = 0;
-	int i;
-	int j;
-	for (i = 0; i < 10; i++)
+
+	for (int i = 0 ; i < 10; i++)
 	{
-		for (j = i; j < 10; j++)
+		putchar('0' + i);
+		if (i < 9)
 		{
-			if (comma)
-			{
-				putchar(',');
-				
-			}
-			putchar('0' + i);
-			putchar('0' + j);
-			comma = 1;
-		}
+			putchar(',');
+			putchar(' ');
+        	}
 	}
 	putchar('\n');
 
