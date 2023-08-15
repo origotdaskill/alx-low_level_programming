bad78af6e915742f8824724c0f65fd36349a1a4f
origotdaskill <mr.walid.ori@gmail.com>
2023-08-15 14:32:39 -0700

    new commit 0x07

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.cc
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description:a program that prints the lowercase alphabe.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+}
