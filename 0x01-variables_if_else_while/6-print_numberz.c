7badca4b6bcce560a88c69ac0e0c36fc2baa5150
origotdaskill <mr.walid.ori@gmail.com>
2023-08-15 14:17:39 -0700

    new commit 0x06

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,17 +10,11 @@
  */
 int main(void)
 {
-	putchar('0' + 0);
-	putchar('0' + 1);
-	putchar('0' + 2);
-	putchar('0' + 3);
-	putchar('0' + 4);
-	putchar('0' + 5);
-	putchar('0' + 6);
-	putchar('0' + 7);
-	putchar('0' + 8);
-	putchar('0' + 9);
-	putchar('\n');
+	for (int i = 0; i < 10; i++)
+	{
+	putchar('0'+i);
+	}
+	 putchar('\n');
 
 	return (0);
 }
