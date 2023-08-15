04991f96fa18195c7f06dc6fe4aec61b7030a8fe
origotdaskill <mr.walid.ori@gmail.com>
2023-08-15 16:11:24 -0700

    new commit 0x09

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,25 +9,24 @@
  */
 int main(void)
 {
+	int comma = 0;
 	int i;
 	int j;
-
 	for (i = 0; i < 10; i++)
 	{
 		for (j = i; j < 10; j++)
 		{
-			putchar('0' + i);
-			putchar(',');
-			putchar(' ');
-			putchar('0' + j);
-			if (i != 9 || j != 9)
+			if (comma)
 			{
 				putchar(',');
 				putchar(' ');
 			}
+			putchar('0' + i);
+			putchar('0' + j);
+			comma = 1;
 		}
 	}
 	putchar('\n');
-	
+
 	return (0);
 }
