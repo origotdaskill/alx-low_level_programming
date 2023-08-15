863af809762c3fe48a9c5d81ca1fb00a20cf0e39
origotdaskill <mr.walid.ori@gmail.com>
2023-08-15 08:21:16 -0700

    new commit 0x04

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
 
-/**
- * main - Entry point of the program
- *
- *a program that prints all possible combinations of single-digit numbers..
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-	putchar('0');
-	for (int i = 1; i <= 9; i++)
-	{
-	putchar(',');
- 	putchar(' ');
-	putchar(i + '0'); }
-	putchar('\n');
+    char letter;
 
-	return (0);
+    for (letter = 'a'; letter <= 'z'; letter++)
+    {
+        if (letter != 'q' && letter != 'e')
+        {
+            putchar(letter);
+        }
+    }
+    putchar('\n');
+
+    return (0);
 }
