ff28ecb55b7c0f694bce9ab0d8f835a546e75184
origotdaskill <mr.walid.ori@gmail.com>
2023-08-17 08:19:54 -0700

    new commit 0x00

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point of the program
+ *
+ * Description: Generates a random number and determines if it's positive,
+ *              negative, or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int positive_or_negative(int n)
+{
+        
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        /* your code goes there */
+        printf("%d ", n);
+        if (n > 0)
+        {printf("is positive\n"); }
+        else if (n == 0)
+        {printf("is zero\n"); }
+        else
+        {printf("is negative\n"); }
+        return (0);
+}
