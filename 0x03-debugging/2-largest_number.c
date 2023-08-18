a8b7cf24c51fd38556801ad08032a4fffec7b599
origotdaskill <mr.walid.ori@gmail.com>
2023-08-18 06:20:50 -0700

    new commit 0x01

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -7,7 +7,6 @@
  * @c: third integer
  * Return: largest number
  */
-
 int largest_number(int a, int b, int c)
 {
 	int largest;
@@ -27,4 +26,3 @@ int largest_number(int a, int b, int c)
 
 	return (largest);
 }
-
