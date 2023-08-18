246203a801e3d59a7cf0eab81634a036ca08fb99
origotdaskill <mr.walid.ori@gmail.com>
2023-08-18 14:09:38 -0700

    new commit 0x00

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,13 @@
 #include "main.h"
 
+/**
+ *
+ *print_alphabetr : print all alphabet function .
+ */
 void print_alphabet(void)
 {
 	char letter = 'a';
+	
 	while (letter <= 'z')
 	{
 	_putchar(letter);
