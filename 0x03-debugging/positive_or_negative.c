0f2ad88c1948a745cb78a720392f72b8dfd67171
origotdaskill <mr.walid.ori@gmail.com>
2023-08-18 05:42:35 -0700

    new commit 0x00

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -13,16 +13,21 @@
 /* betty style doc for function main goes there */
 int positive_or_negative(int n)
 {
-        
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        /* your code goes there */
-        printf("%d ", n);
-        if (n > 0)
-        {printf("is positive\n"); }
-        else if (n == 0)
-        {printf("is zero\n"); }
-        else
-        {printf("is negative\n"); }
-        return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	printf("%d ", n);
+	if (n > 0)
+	{
+		printf("is positive\n");
+	}
+	else if (n == 0)
+        {
+		printf("is zero\n");
+	}
+	else
+	{
+		printf("is negative\n");
+	}
+	return (0);
 }
