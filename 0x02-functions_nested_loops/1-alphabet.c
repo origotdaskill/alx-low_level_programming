801c29f152982797f4f47d3d480c4575e4bb8e0a
origotdaskill <mr.walid.ori@gmail.com>
2023-08-18 14:19:16 -0700

    new commit 0x00

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
-/*
- *print_alphabetr : print all alphabet function .
+/**
+ * print_alphabet - Prints the lowercase alphabet followed by a new line
  */
 void print_alphabet(void)
 {
 	char letter = 'a';
-	
 	while (letter <= 'z')
 	{
-	_putchar(letter);
-	letter++;
+		_putchar(letter);
+		letter++;
 	}
 	_putchar('\n');
 }
