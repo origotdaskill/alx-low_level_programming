f4dd993c19b08d03eefcfa945074aa834520c6d6
origotdaskill <mr.walid.ori@gmail.com>
2023-08-19 04:51:10 -0700

    new commit 0x03

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints the numbers from 0 to 9 followed by a new line
+ */
+void print_numbers(void)
+{
+	char digit = '0';
+
+	while (digit <= '9')
+	{
+		_putchar(digit);
+		digit++;	
+	}
+	_putchar('\n');
+}
