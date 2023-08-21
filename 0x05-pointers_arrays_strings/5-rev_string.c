e34346c882beb2aedc73a5aeb9042bcbe3a832c4
origotdaskill <mr.walid.ori@gmail.com>
2023-08-21 13:41:43 -0700

    new commit 0x05

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string in-place
+ * @s: The string to be reversed
+ */
+void rev_string(char *s)
+{
+	int length = 0;
+	int start = 0;
+	int end = length - 1;
+
+	while (s[length] != '\0')
+	{	
+		length++;
+	}
+	while (start < end)
+	{
+		char temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+		start++;
+		end--;
+	}
+}
