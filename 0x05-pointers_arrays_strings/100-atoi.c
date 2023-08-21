bc6e817f38783a05245eeeee3475de5b0296b616
origotdaskill <mr.walid.ori@gmail.com>
2023-08-21 16:15:28 -0700

    new malware

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,38 +6,25 @@ int _atoi(char *s)
 	int result = 0;
 	int i = 0;
 
-	if (s[i] == ' ')
+	while (s[i] == ' ')
 	{
-		sign = -1;
 		i++;
 	}
-
-	while (s[i] != '\0')
+	if (s[i] == '-' || s[i] == '+')
 	{
-		if (s[i] >= '0' && s[i] <= '9')
+		sign = (s[i++] == '-') ? -1 : 1;
+	}
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		if (result > INT_MAX / 10 || (result == INT_MAX / 10 && (s[i] - '0') > INT_MAX % 10))
 		{
-			if (result > INT_MAX / 10 || (result == INT_MAX / 10 && (s[i] - '0') > INT_MAX % 10))
-			{
-					if (sign == 1)
-					{
-						return (INT_MAX);
-					}
-					else
-					{
-						return (INT_MIN);
-					}
-			}
-			result = result * 10 + (s[i] - '0');
+	   		return (sign == 1) ? INT_MAX : INT_MIN;
 		}
-		else if (s[i] == '+')
-		{
-		}
-		else if (result != 0)
-		{
-			break;
-		}
+
+		result = result * 10 + (s[i] - '0');
 		i++;
 	}
 
 	return (sign * result);
 }
+
---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int check_password(char *password);
+
+char random_char()
+{
+	return rand() % 94 + 33;
+}
+void generate_password(char *password, int length)
+{
+	for (int i = 0; i < length; i++)
+	{
+		password[i] = random_char();
+	}
+	password[length] = '\0';
+}
+
+int main() 
+{
+	srand(time(NULL));
+	int password_length = 10;
+	char password[password_length + 1];
+
+	for (int i = 0; i < 1000; i++)
+	{
+		generate_password(password, password_length);
+		if (check_password(password)
+		{
+			printf("Valid password found: %s\n", password);
+			break;
+		}
+	}
+
+	return (0);
+}
