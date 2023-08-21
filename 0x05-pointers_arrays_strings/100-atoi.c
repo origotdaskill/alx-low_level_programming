af0742088604878ffdc6baaff9bb1b691bc40a8b
origotdaskill <mr.walid.ori@gmail.com>
2023-08-21 15:39:51 -0700

    new commit 0x100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,37 +1,31 @@
-#include "main.h"
 
-/**
- * _atoi - Converts a string to an integer
- * @s: The string to be converted
- * Return: The converted integer
- */
+
 int _atoi(char *s)
 {
 	int sign = 1;
 	int result = 0;
 	int i = 0;
 
-	if (s[i] == '-')
+	if (s[0] == '-')
 	{
 		sign = -1;
 		i++;
 	}
-	else if (s[i] == '+')
-	{
-		i++;
-	}
 	while (s[i] != '\0')
 	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result = result * 10 + (s[i] - '0');
-		}
-		else
-		{
-			break;
-		}
-		i++;
+	if (s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
+	}
+	else if (s[i] == '+')
+	{
+       	}
+	else
+	{
+		break;
+	}
+	i++;
 	}
 
-	return (result * sign);
+	return sign * result;
 }
