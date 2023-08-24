94aa74b8179a0e14c9a98ed71be94221473f5a04
origotdaskill <mr.walid.ori@gmail.com>
2023-08-24 08:23:59 -0700

    new commit 0x100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using rot13.
+ * @str: The string to encode.
+ * Return: A pointer to the modified string.
+ */
+char *rot13(char *str)
+{
+	char *input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *output = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; input[j] != '\0'; j++)
+		{
+			if (str[i] == input[j])
+			{
+				str[i] = output[j];
+				break;
+			}
+		}	
+	}
+	return (str);
+}
