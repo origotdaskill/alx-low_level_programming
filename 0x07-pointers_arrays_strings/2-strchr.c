2861c9bac6b2034dcfeda86c0590d25678c9559d
origotdaskill <mr.walid.ori@gmail.com>
2023-08-28 06:46:58 -0700

    new commit 0x02

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 /**
  * _strchr - Locates a character in a string.
  * @s: Pointer to the string to search.
@@ -10,12 +11,16 @@ char *_strchr(char *s, char c)
 	while (*s != '\0')
 	{
 		if (*s == c)
+		{
 			return (s);
+		
+		}
 		s++;
 	}
 
 	if (*s == c)
+	{
 		return (s);
-
+	}
 	return (NULL);
 }
