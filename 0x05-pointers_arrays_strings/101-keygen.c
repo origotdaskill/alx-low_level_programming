07873c7ce9d1399b9360fc5ded2aa6120757d2d2
origotdaskill <mr.walid.ori@gmail.com>
2023-08-30 16:47:01 -0700

    Please enter the commit message for your changes. Lines starting

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -28,7 +28,7 @@ int main()
 	for (i = 0; i < 1000; i++)
 	{
 		generate_password(password, password_length);
-		if (check_password(password)
+		if (check_password(password))
 		{
 			printf("Valid password found: %s\n", password);
 			break;
@@ -36,4 +36,20 @@ int main()
 	}
 
 	return (0);
+}
+bool check_password(char *password) {
+    // Your logic to check the validity of the password goes here
+    // For example, you can compare the password against some criteria
+
+    // For demonstration purposes, let's assume a simple criteria
+    // where the password should be at least 8 characters long
+    int length = 0;
+    while (password[length] != '\0') {
+        length++;
+    }
+    if (length >= 8) {
+        return true; // Password is valid
+    } else {
+        return false; // Password is not valid
+    }
 }
