96831030e89310970aac60ca6a1183efbcf753f6
origotdaskill <mr.walid.ori@gmail.com>
2023-09-01 18:45:13 -0700

    new commit 0x03

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/**
- * main - Entry point of the program
- * @argc: The number of command-line arguments
- * @argv: An array containing the command-line arguments
- *
- * Return: Always 0 (success)
- */
-int main(int argc, char *argv[]) 
-{	
-	int i;
+int main(int argc, char *argv[])
+{
+	int num1, num2, result; 
 
-	for (i = 0; i < argc; i++)
+	if (argc != 3)
 	{
-		printf("%s\n", argv[i]);
+		printf("Error\n");
+		return (1);
 	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
 	return (0);
 }
