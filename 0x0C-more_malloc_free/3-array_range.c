5b5a660948a47bf8aa3cd09737c6c574805ccade
origotdaskill <mr.walid.ori@gmail.com>
2023-09-07 13:59:31 -0700

    new commit 0x04

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+
+/**
+ * array_range - creates an array of integers from min to max
+ * @min: the minimum value
+ * @max: the maximum value
+ *
+ * Return: pointer to the newly created array, or NULL on failure
+ */
+int *array_range(int min, int max)
+{
+	int *arr;
+	int size, i;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	size = max - min + 1;
+
+	arr = malloc(sizeof(int) * size);
+
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = min + i;
+	}
+
+	return (arr);
+}
