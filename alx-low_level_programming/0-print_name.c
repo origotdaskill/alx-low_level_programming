30d2a9d2926402cda9b8981fbbad5a5b324c4c09
origotdaskill <mr.walid.ori@gmail.com>
2023-09-14 15:21:50 -0700

    new commit 0x00

---
+++ alx-low_level_programming/0-print_name.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * @brief Function to print a name using a given function.
+ *
+ * This function takes a name as input and uses a provided function pointer
+ * to print the name in a specific format.
+ *
+ * @param name The name to be printed.
+ * @param f A function pointer that specifies the print format.
+ */
+void print_name(char *name, void (*f)(char *)) {
+    f(name);
+}
+
+/**
+ * @brief Function to print the name in uppercase.
+ *
+ * This function takes a name and prints it in uppercase letters.
+ *
+ * @param name The name to be printed in uppercase.
+ */
+void print_uppercase(char *name) {
+    char *ptr = name;
+    while (*ptr) {
+        putchar(toupper(*ptr));
+        ptr++;
+    }
+    putchar('\n');
+}
+
+/**
+ * @brief Function to print the name in lowercase.
+ *
+ * This function takes a name and prints it in lowercase letters.
+ *
+ * @param name The name to be printed in lowercase.
+ */
+void print_lowercase(char *name) {
+    char *ptr = name;
+    while (*ptr) {
+        putchar(tolower(*ptr));
+        ptr++;
+    }
+	putchar('\n');
+}
---
+++ alx-low_level_programming/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+void print_lowercase(char *name);
+void print_uppercase(char *name);
+void print_name(char *name, void (*f)(char *));
+
+#endif /* FUNCTION_POINTERS_H */
