6189d6247271bb1cc01fd9a828a571a4799d93d5
origotdaskill <mr.walid.ori@gmail.com>
2023-09-22 17:16:50 -0700

    new commit 100x0

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void startup_message(void)
+{
+    printf("You're beat! and yet, you must allow,\n");
+    printf("I bore my house upon my back!\n");
+}
---
+++ 0x12-singly_linked_lists/100-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void startup_message(void);
+
+int main(void)
+{
+	startup_message();
+    	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+   	return (0);
+}
