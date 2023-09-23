5f7b3886a1ef865f2032bb852185ca83e2fc0c95
origotdaskill <mr.walid.ori@gmail.com>
2023-09-22 17:22:32 -0700

    new commit 0x100

--- 0x12-singly_linked_lists/100-main.c
+++ 0x12-singly_linked_lists/100-main.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
+/**
+ * startup_message - Prints a startup message.
+ */
+void startup_message(void);
 
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-
-void startup_message(void);
-
 int main(void)
 {
 	startup_message();
-    	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-   	return (0);
+	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+	return (0);
 }
