a92f09899f161b9a18196b280ef9c14b3d2df0cb
origotdaskill <mr.walid.ori@gmail.com>
2023-09-26 11:45:24 -0700

    ori commit 0x06

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * pop_listint - Deletes the head node of a listint_t linked list.
+ * @head: Pointer to a pointer to the head of the list
+ *
+ * Return: The data (n) of the deleted head node, or 0 if the list is empty
+ */
+int pop_listint(listint_t **head)
+{
+    int data = 0;
+
+	if (*head != NULL)
+	{
+		listint_t *temp = *head;
+		data = temp->n;
+		*head = (*head)->next;
+		free(temp);
+	}
+
+	return (data);
+}
