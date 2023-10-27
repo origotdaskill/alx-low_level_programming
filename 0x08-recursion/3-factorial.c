0b3de60267bad7d1ded3bb17bace69eb8f2b2e6d
origotdaskill <mr.walid.ori@gmail.com>
2023-10-27 01:42:06 +0100

    new commit 0102

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * factorial - Returns the factorial of a given number.
+ * @n: The number for which to calculate the factorial.
+ *
+ * Return: The factorial of n, or -1 if n is less than 0.
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-)1;
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
