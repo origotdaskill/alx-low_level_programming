a66b686cc7a17cfbc76e70d31a171b80884a3658
origotdaskill <mr.walid.ori@gmail.com>
2023-10-28 00:58:35 +0100

    new commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,33 +1,34 @@
 /**
- * is_prime_number - Check if the input integer is a prime number.
- * @n: The integer to be checked.
+ * is_divisible - Check if a number is divisible.
+ * @n: The number to check.
+ * @i: The divisor.
  *
- * Return: 1 if n is a prime number, 0 otherwise.
+ * Return: 1 if it's a prime number, 0 otherwise.
+ */
+int is_divisible(int n, int i)
+{
+	if (i * i > n)
+		return (1);
+	if (n % i == 0 || n % (i + 2) == 0)
+		return (0);
+
+	return (is_divisible(n, i + 6));
+}
+
+/**
+ * is_prime_number - Check if a number is prime.
+ * @n: The number to check.
+ *
+ * Return: 1 if it's a prime number, 0 otherwise.
  */
 int is_prime_number(int n)
 {
-	int i ;
-
 	if (n <= 1)
-	{
 		return (0);
-	}
 	if (n <= 3)
-	{
 		return (1);
-	}
 	if (n % 2 == 0 || n % 3 == 0)
-	{
 		return (0);
-	}
 	
-	i = 5;
-	while ( i * i <= n)
-	{
-		if (n % i == 0 || n % (i + 2) == 0)
-		{
-			return (0);
-		}
-	}
-	return (1);
+	return (is_divisible(n, 5));
 }
