75463e26d5254fbcdd298423e9c54205b7aa0b62
origotdaskill <mr.walid.ori@gmail.com>
2023-10-27 01:12:10 +0100

    new commit 0102

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,17 @@
-#include <stdio.h>
+#include "main.h"
 
-/*
- *_puts_recursion is function 
- * to give a factorial of @char
- * */
-
+/**
+ * _puts_recursion - Prints a string followed by a new line.
+ * @s: The string to be printed.
+ */
 void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		putchar(*s);
-	}
-	else 
-	{
-		putchar('\n');
-	}
+    if (*s == '\0')
+    {
+        _putchar('\n');
+        return;
+    }
+
+    _putchar(*s);
+    _puts_recursion(s + 1);
 }
