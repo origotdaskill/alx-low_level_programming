cda4a43968566efdcb6a53fa9648d54a01f0389a
origotdaskill <mr.walid.ori@gmail.com>
2023-10-30 06:31:30 -0700

    ori commit 0x00

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+/**                                                                
+ * main - Entry point of the program                                      
+ *                                                                        
+ * Description: This program takes command-line arguments and prints them
+ * one per line, including the program name itself.
+ *                                                 
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of strings containing the arguments.
+ *
+ * Return: 0 if the program runs successfully.
+ */
+int main(int argc, char *argv[])
+{
+	for (int i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return 0;
+}
