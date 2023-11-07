d00a08b13e656b4d28ebd2756da6212ee40631a2
origotdaskill <mr.walid.ori@gmail.com>
2023-11-07 06:53:49 -0800

    ori commit 0x04

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -56,4 +56,13 @@ void print_dog(struct dog *d);
  * makes copies of the provided name and owner to store in the dog structure.
  */
 dog_t *new_dog(char *name, float age, char *owner);
+/**
+ * free_dog - Frees a dog and its associated memory
+ * @d: Pointer to a dog_t structure to be freed
+ *
+ * Description: This function frees the memory allocated for a dog, including
+ * the memory allocated for the name and owner strings. It ensures there are
+ * no memory leaks when the dog is no longer needed.
+ */
+void free_dog(dog_t *d);
 #endif /* DOG_H */
