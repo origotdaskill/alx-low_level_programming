ec29a224955620184f3f754b6195f4dc1ada31fc
origotdaskill <mr.walid.ori@gmail.com>
2023-11-16 20:51:37 +0100

    ori commit

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node_end - Adds a new node at the end of a list_t list
+ * @head: Pointer to the pointer of the head of the list
+ * @str: String to be duplicated and set as the node's str
+ * Return: The address of the new element, or NULL if it failed
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+    list_t *new_node, *temp;
+
+    if (head == NULL || str == NULL)
+        return NULL;
+
+    /* Allocate memory for the new node */
+    new_node = malloc(sizeof(list_t));
+    if (new_node == NULL)
+        return NULL;
+
+    /* Duplicate the string and set the node's attributes */
+    new_node->str = strdup(str);
+    if (new_node->str == NULL)
+    {
+        free(new_node);
+        return NULL;
+    }
+
+    /* Set the length of the string */
+    new_node->len = strlen(str);
+
+    /* Set the next pointer to NULL as it will be the last node */
+    new_node->next = NULL;
+
+    /* If the list is empty, set the head to the new node */
+    if (*head == NULL)
+    {
+        *head = new_node;
+        return new_node;
+    }
+
+    /* Traverse the list to find the last node */
+    temp = *head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+
+    /* Set the next pointer of the last node to the new node */
+    temp->next = new_node;
+
+    return new_node;
+}
