f8ce819412e6337771253d4077417aa11f53496f
origotdaskill <mr.walid.ori@gmail.com>
2023-11-20 21:02:01 +0100

    ori commit

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,13 @@
+#ifndef YOUR_HEADER_H
+#define YOUR_HEADER_H
+
+/* Define your struct */
+typedef struct listint_t {
+    int n;
+    struct listint_t *next;
+} listint_t;
+
+/* Function prototypes */
+size_t print_listint(const listint_t *h);
+
+#endif /* YOUR_HEADER_H */
