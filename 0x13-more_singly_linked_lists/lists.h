4abdd9dee6498cbed29cd8c79675bf5ff4e1857c
origotdaskill <mr.walid.ori@gmail.com>
2023-11-21 08:01:56 -0800

    ori commit

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,5 +18,6 @@ listint_t *add_nodeint_end(listint_t **head, int n);
 void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
 int pop_listint(listint_t **head);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 
 #endif /* YOUR_HEADER_H */
