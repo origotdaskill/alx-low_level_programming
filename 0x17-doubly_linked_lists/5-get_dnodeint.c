a6cfccd4d3bcc5021472f62dce3543393d40349f
origotdaskill <mr.walid.ori@gmail.com>
2024-01-11 20:15:24 +0100

    ori commit

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
+ * @head: pointer to the head of the list
+ * @index: index of the node to retrieve
+ *
+ * Return: pointer to the nth node, or NULL if the node does not exist
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+    unsigned int i;
+
+    for (i = 0; i < index && head != NULL; i++)
+        head = head->next;
+
+    return (head);
+}
