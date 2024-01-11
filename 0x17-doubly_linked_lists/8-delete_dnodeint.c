815b10e9f3cd27289cb1c793c3bc1edfbe077d77
origotdaskill <mr.walid.ori@gmail.com>
2024-01-11 20:22:41 +0100

    ori commit

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes the node at index of a dlistint_t list
+ * @head: pointer to the pointer to the head of the list
+ * @index: index of the node to delete
+ *
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+    dlistint_t *temp, *prev;
+    unsigned int i = 0;
+
+    if (head == NULL || *head == NULL)
+        return (-1);
+
+    temp = *head;
+
+    if (index == 0)
+    {
+        *head = temp->next;
+        if (*head != NULL)
+            (*head)->prev = NULL;
+        free(temp);
+        return (1);
+    }
+
+    while (temp != NULL && i < index)
+    {
+        prev = temp;
+        temp = temp->next;
+        i++;
+    }
+
+    if (temp == NULL)
+        return (-1);
+
+    prev->next = temp->next;
+    if (temp->next != NULL)
+        temp->next->prev = prev;
+
+    free(temp);
+    return (1);
+}
