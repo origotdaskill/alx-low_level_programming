838e4d96b75cb705780f52c9c2e09119d1d14acb
origotdaskill <mr.walid.ori@gmail.com>
2024-02-01 21:17:34 +0100

    ori commit

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,62 @@
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: the hash table to add or update the key/value to
+ * @key: the key
+ * @value: the value associated with the key
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+    unsigned long int index;
+    hash_node_t *new_node, *current_node;
+
+    if (ht == NULL || key == NULL || strcmp(key, "") == 0 || value == NULL)
+        return (0);
+
+    index = key_index((unsigned char *)key, ht->size);
+    current_node = ht->array[index];
+
+    /* Check if key already exists, update value if it does */
+    while (current_node != NULL)
+    {
+        if (strcmp(current_node->key, key) == 0)
+        {
+            free(current_node->value);
+            current_node->value = strdup(value);
+            if (current_node->value == NULL)
+                return (0);
+            return (1);
+        }
+        current_node = current_node->next;
+    }
+
+    /* Key doesn't exist, create a new node */
+    new_node = malloc(sizeof(hash_node_t));
+    if (new_node == NULL)
+        return (0);
+
+    new_node->key = strdup(key);
+    if (new_node->key == NULL)
+    {
+        free(new_node);
+        return (0);
+    }
+
+    new_node->value = strdup(value);
+    if (new_node->value == NULL)
+    {
+        free(new_node->key);
+        free(new_node);
+        return (0);
+    }
+
+    new_node->next = ht->array[index];
+    ht->array[index] = new_node;
+
+    return (1);
+}
